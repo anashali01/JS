b54528e8a3d1b9891baca731d8fcc09de1ba05ab
anashali01 <saiyedanashali@gmail.com>
2025-06-30 10:18:32 +0530

    first commit

---
+++ Day-1-OPERATORS/script/script.js
@@ -0,0 +1,40 @@
+// OPERATORS
+
+// Arithmetic operators
+let a = 10;
+let b = 5;
+
+console.log(`---- Arithmetic operators ----`);
+console.log(`Addition: ${a + b}`);
+console.log(`Subtraction: ${a - b}`);
+console.log(`Multiplication: ${a * b}`);
+console.log(`Division: ${a / b}`);
+console.log(`Modulus: ${a % b}`);
+console.log(`Power: ${a ** b}`);
+
+// Assignment operators
+let x = 10;
+console.log(`---- Assignment operators ----`);
+console.log(x+=5);
+console.log(x-=5);
+console.log(x*=5);
+console.log(x/=5);
+console.log(x%=5);
+
+// Comparison operators
+let y = 10;
+console.log(`---- Comparison operators ----`);
+console.log(`Equal: ${y==10}`);
+console.log(`Not Equal: ${y!=10}`);
+console.log(`Greater Or Equal: ${y>=100}`);
+console.log(`Less Or Equal: ${y<=10}`);
+
+// Logical operators
+let num1 = 10;
+let num2 = 20;
+console.log(`---- Logical operators ----`);
+console.log(`AND: ${num1 > 5 && num2 < 30}`);
+console.log(`OR: ${num1 > 5 || num2 <= 10}`);
+console.log(`NOT: ${num1 == num2}`);
+
+
